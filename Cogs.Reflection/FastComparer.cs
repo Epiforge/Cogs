cf1a89217a18f70b2418cbbb3104ecb3a0c1bce2
Daniel Henry <daniel.henry@epiforge.com>
2020-02-29 02:11:25 -0500

    Reflection: added FastComparer, FastDefault, FastEqualityComparer, FastMethodInfo

---
+++ Cogs.Reflection/FastComparer.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Cogs.Reflection
+{
+    /// <summary>
+    /// Provides a method for comparing instances of a type that is not known at compile time
+    /// </summary>
+    public class FastComparer
+    {
+        FastComparer(Type type)
+        {
+            Type = type;
+            var comparerType = typeof(Comparer<>).MakeGenericType(type);
+            comparer = comparerType.GetRuntimeProperty(nameof(Comparer<object>.Default)).GetValue(null);
+            compare = FastMethodInfo.Get(comparerType.GetRuntimeMethod(nameof(Comparer<object>.Compare), new Type[] { type }));
+        }
+
+        readonly FastMethodInfo compare;
+        readonly object comparer;
+
+        /// <summary>
+        /// Gets the type for which this <see cref="FastComparer"/> makes comparisons
+        /// </summary>
+        public Type Type { get; }
+
+        /// <summary>
+        /// Performs a case-sensitive comparison of two objects of the type indicated by <see cref="Type"/> and returns a value indicating whether one is less than, equal to, or greater than the other
+        /// </summary>
+        /// <param name="x">The first object to compare</param>
+        /// <param name="y">The second object to compare</param>
+        /// <returns>
+        /// A signed integer that indicates the relative values of <paramref name="x"/> and <paramref name="y"/>, as shown in the following table:
+        /// <table>
+        ///     <tr>
+        ///         <th>Value</th>
+        ///         <th>Meaning</th>
+        ///     </tr>
+        ///     <tr>
+        ///         <td>Less than zero</td>
+        ///         <td><paramref name="x"/> is less than <paramref name="y"/></td>
+        ///     </tr>
+        ///     <tr>
+        ///         <td>Zero</td>
+        ///         <td><paramref name="x"/> equals <paramref name="y"/></td>
+        ///     </tr>
+        ///     <tr>
+        ///         <td>Less than zero</td>
+        ///         <td><paramref name="x"/> is greater than <paramref name="y"/></td>
+        ///     </tr>
+        /// </table>
+        /// </returns>
+        public int Compare(object? x, object? y) => compare.Invoke(comparer, x, y) is int comparison ? comparison : throw new Exception("Comparison failed");
+
+        static readonly ConcurrentDictionary<Type, FastComparer> comparers = new ConcurrentDictionary<Type, FastComparer>();
+
+        /// <summary>
+        /// Gets a <see cref="FastComparer"/> for the specified type
+        /// </summary>
+        /// <param name="type">The type</param>
+        /// <returns>A <see cref="FastComparer"/></returns>
+        public static FastComparer Create(Type type)
+        {
+            if (type is null)
+                throw new ArgumentNullException(nameof(type));
+            return comparers.GetOrAdd(type, Factory);
+        }
+
+        static FastComparer Factory(Type type) => new FastComparer(type);
+    }
+}
---
+++ Cogs.Reflection/FastMethodInfo.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Cogs.Reflection
+{
+    /// <summary>
+    /// Provides a method for invoking a method that is not known at compile time
+    /// </summary>
+    public class FastMethodInfo
+    {
+        private delegate object? ReturnValueDelegate(object? instance, object?[] arguments);
+        private delegate void VoidDelegate(object? instance, object?[] arguments);
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FastMethodInfo"/> class based on the specified <see cref="System.Reflection.MethodInfo"/>
+        /// </summary>
+        /// <param name="methodInfo">The <see cref="System.Reflection.MethodInfo"/> reflecting the method to be invoked</param>
+        FastMethodInfo(MethodInfo methodInfo)
+        {
+            if (methodInfo is null)
+                throw new ArgumentNullException(nameof(methodInfo));
+            MethodInfo = methodInfo;
+            var instanceExpression = Expression.Parameter(typeof(object), "instance");
+            var argumentsExpression = Expression.Parameter(typeof(object[]), "arguments");
+            var argumentExpressions = new List<Expression>();
+            var parameterInfos = methodInfo.GetParameters();
+            for (var i = 0; i < parameterInfos.Length; ++i)
+            {
+                var parameterInfo = parameterInfos[i];
+                argumentExpressions.Add(Expression.Convert(Expression.ArrayIndex(argumentsExpression, Expression.Constant(i)), parameterInfo.ParameterType));
+            }
+            var callExpression = Expression.Call(!methodInfo.IsStatic ? Expression.Convert(instanceExpression, methodInfo.DeclaringType) : null, methodInfo, argumentExpressions);
+            if (callExpression.Type == typeof(void))
+            {
+                var voidDelegate = Expression.Lambda<VoidDelegate>(callExpression, instanceExpression, argumentsExpression).Compile();
+                @delegate = (instance, arguments) => { voidDelegate(instance, arguments); return null; };
+            }
+            else
+                @delegate = Expression.Lambda<ReturnValueDelegate>(Expression.Convert(callExpression, typeof(object)), instanceExpression, argumentsExpression).Compile();
+        }
+
+        readonly ReturnValueDelegate @delegate;
+
+        /// <summary>
+        /// Invokes the method reflected by <see cref="MethodInfo"/>
+        /// </summary>
+        /// <param name="instance">The object on which to invoke the method (if a method is static, this argument is ignored)</param>
+        /// <param name="arguments">An argument list for the invoked method</param>
+        /// <returns>An object containing the return value of the invoked method</returns>
+        public object? Invoke(object? instance, params object?[] arguments)
+        {
+            if (arguments is null)
+                throw new ArgumentNullException(nameof(arguments));
+            return @delegate(instance, arguments);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="System.Reflection.MethodInfo"/> reflecting the method this <see cref="FastMethodInfo"/> will invoke
+        /// </summary>
+        public MethodInfo MethodInfo { get; }
+
+        static readonly ConcurrentDictionary<MethodInfo, FastMethodInfo> fastMethodInfos = new ConcurrentDictionary<MethodInfo, FastMethodInfo>();
+
+        static FastMethodInfo Create(MethodInfo methodInfo) => new FastMethodInfo(methodInfo);
+
+        /// <summary>
+        /// Get a <see cref="FastMethodInfo"/> for the specified <see cref="System.Reflection.MethodInfo" />
+        /// </summary>
+        /// <param name="methodInfo">The <see cref="MethodInfo"/></param>
+        /// <returns>A <see cref="FastMethodInfo"/></returns>
+        public static FastMethodInfo Get(MethodInfo methodInfo)
+        {
+            if (methodInfo is null)
+                throw new ArgumentNullException(nameof(methodInfo));
+            return fastMethodInfos.GetOrAdd(methodInfo, Create);
+        }
+    }
+}
