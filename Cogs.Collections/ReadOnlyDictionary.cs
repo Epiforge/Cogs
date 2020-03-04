50c852459e2cd3398765d36f9733ac194ef4b64c
Daniel Henry <daniel.henry@epiforge.com>
2020-03-04 18:21:47 -0500

    Collections: added ReadOnlyDictionary, ReadOnlyRangeDictionary, ReadOnlyObservableRangeDictionary; minor increment

---
+++ Cogs.Collections/ReadOnlyDictionary.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Cogs.Collections
+{
+    /// <summary>
+    /// Read-only wrapper around an <see cref="IReadOnlyDictionary{TKey, TValue}"/>
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the read-only dictionary</typeparam>
+    /// <typeparam name="TValue">The type of values in the read-only dictionary</typeparam>
+    public class ReadOnlyDictionary<TKey, TValue> : IReadOnlyDictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadOnlyDictionary{TKey, TValue}"/> class
+        /// </summary>
+        /// <param name="readOnlyDictionary">The <see cref="IReadOnlyDictionary{TKey, TValue}"/> around which to wrap</param>
+        public ReadOnlyDictionary(IReadOnlyDictionary<TKey, TValue> readOnlyDictionary) => this.readOnlyDictionary = readOnlyDictionary;
+
+        readonly IReadOnlyDictionary<TKey, TValue> readOnlyDictionary;
+
+        /// <summary>
+        /// Determines whether the read-only dictionary contains an element that has the specified key
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <returns><c>true</c> if the dictionary contains the key; otherwise, <c>false</c></returns>
+        public bool ContainsKey(TKey key) => readOnlyDictionary.ContainsKey(key);
+
+        IEnumerator IEnumerable.GetEnumerator() => readOnlyDictionary.GetEnumerator();
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the read-only dictionary
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the dictionary</returns>
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator() => readOnlyDictionary.GetEnumerator();
+
+        /// <summary>
+        /// Gets the value that is associated with the specified key
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <param name="value">When this method returns, the value associated with the specified key, if the key is found; otherwise, the default value for the type of the value parameter</param>
+        /// <returns><c>true</c> if the read-only dictionary contains the key; otherwise, <c>false</c></returns>
+        public bool TryGetValue(TKey key, out TValue value) => readOnlyDictionary.TryGetValue(key, out value);
+
+        /// <summary>
+        /// Gets the element that has the specified key in the read-only dictionary
+        /// </summary>
+        /// <param name="key">The key to locate</param>
+        /// <returns>The element that has the specified key in the read-only dictionary</returns>
+        public TValue this[TKey key] => readOnlyDictionary[key];
+
+        /// <summary>
+        /// Gets the number of elements in the read-only dictionary
+        /// </summary>
+        public int Count => readOnlyDictionary.Count;
+
+        /// <summary>
+        /// Gets an enumerable collection that contains the keys in the read-only dictionary
+        /// </summary>
+        public IEnumerable<TKey> Keys => readOnlyDictionary.Keys;
+
+        /// <summary>
+        /// Gets an enumerable collection that contains the values in the read-only dictionary
+        /// </summary>
+        public IEnumerable<TValue> Values => readOnlyDictionary.Values;
+    }
+}
---
+++ Cogs.Collections/ReadOnlyRangeDictionary.cs
@@ -0,0 +1,86 @@
+using System;
+using System.Collections.Generic;
+
+namespace Cogs.Collections
+{
+    /// <summary>
+    /// Read-only wrapper around an <see cref="IRangeDictionary{TKey, TValue}"/>
+    /// </summary>
+    /// <typeparam name="TKey">The type of keys in the read-only dictionary</typeparam>
+    /// <typeparam name="TValue">The type of values in the read-only dictionary</typeparam>
+    public class ReadOnlyRangeDictionary<TKey, TValue> : ReadOnlyDictionary<TKey, TValue>, IRangeDictionary<TKey, TValue>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReadOnlyRangeDictionary{TKey, TValue}"/> class
+        /// </summary>
+        /// <param name="rangeDictionary">The <see cref="IRangeDictionary{TKey, TValue}"/> around which to wrap</param>
+        public ReadOnlyRangeDictionary(IRangeDictionary<TKey, TValue> rangeDictionary) : base(rangeDictionary) => this.rangeDictionary = rangeDictionary;
+
+        readonly IRangeDictionary<TKey, TValue> rangeDictionary;
+
+        void IDictionary<TKey, TValue>.Add(TKey key, TValue value) => throw new NotSupportedException();
+
+        void ICollection<KeyValuePair<TKey, TValue>>.Add(KeyValuePair<TKey, TValue> item) => throw new NotSupportedException();
+        
+        void IRangeDictionary<TKey, TValue>.AddRange(IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs) => throw new NotSupportedException();
+
+        void IRangeDictionary<TKey, TValue>.AddRange(IReadOnlyList<KeyValuePair<TKey, TValue>> keyValuePairs) => throw new NotSupportedException();
+
+        void ICollection<KeyValuePair<TKey, TValue>>.Clear() => throw new NotSupportedException();
+
+        /// <summary>
+        /// Determines whether the read-only range dictionary contains a key/value pair
+        /// </summary>
+        /// <param name="item">The key/value pair to locate in the read-only range dictionary</param>
+        /// <returns><c>true</c> if the item is found in the read-only range dictionary; otherwise, <c>false</c></returns>
+        public bool Contains(KeyValuePair<TKey, TValue> item) => rangeDictionary.Contains(item);
+
+        /// <summary>
+        /// Copies the elements of the read-only range dictionary to an <see cref="Array"/>, starting at a particular <see cref="Array"/> index
+        /// </summary>
+        /// <param name="array">The one-dimensional <see cref="Array"/> that is the destination of the elements copied from read-only range dictionary (the <see cref="Array"/> must have zero-based indexing)</param>
+        /// <param name="arrayIndex">The zero-based index in array at which copying begins</param>
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex) => rangeDictionary.CopyTo(array, arrayIndex);
+
+        /// <summary>
+        /// Gets the elements with the specified keys
+        /// </summary>
+        /// <param name="keys">The keys of the elements to get</param>
+        /// <returns>The elements with the specified keys</returns>
+        public IReadOnlyList<KeyValuePair<TKey, TValue>> GetRange(IEnumerable<TKey> keys) => rangeDictionary.GetRange(keys);
+
+        bool IDictionary<TKey, TValue>.Remove(TKey key) => throw new NotSupportedException();
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.Remove(KeyValuePair<TKey, TValue> item) => throw new NotSupportedException();
+
+        IReadOnlyList<KeyValuePair<TKey, TValue>> IRangeDictionary<TKey, TValue>.RemoveAll(Func<TKey, TValue, bool> predicate) => throw new NotSupportedException();
+
+        IReadOnlyList<TKey> IRangeDictionary<TKey, TValue>.RemoveRange(IEnumerable<TKey> keys) => throw new NotSupportedException();
+
+        void IRangeDictionary<TKey, TValue>.ReplaceRange(IEnumerable<KeyValuePair<TKey, TValue>> keyValuePairs) => throw new NotSupportedException();
+
+        IReadOnlyList<TKey> IRangeDictionary<TKey, TValue>.ReplaceRange(IEnumerable<TKey> removeKeys, IEnumerable<KeyValuePair<TKey, TValue>> newKeyValuePairs) => throw new NotSupportedException();
+
+        void IRangeDictionary<TKey, TValue>.Reset() => throw new NotSupportedException();
+
+        void IRangeDictionary<TKey, TValue>.Reset(IDictionary<TKey, TValue> dictionary) => throw new NotSupportedException();
+
+        ICollection<TKey> IDictionary<TKey, TValue>.Keys => throw new NotSupportedException();
+
+        ICollection<TValue> IDictionary<TKey, TValue>.Values => throw new NotSupportedException();
+
+        bool ICollection<KeyValuePair<TKey, TValue>>.IsReadOnly => true;
+
+        TValue IDictionary<TKey, TValue>.this[TKey key]
+        {
+            get => rangeDictionary[key];
+            set => throw new NotSupportedException();
+        }
+
+        TValue IRangeDictionary<TKey, TValue>.this[TKey key]
+        {
+            get => rangeDictionary[key];
+            set => throw new NotSupportedException();
+        }
+    }
+}
