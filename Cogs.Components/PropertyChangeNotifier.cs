ab660a310f94cebf838906b820992523384ca305
Daniel Henry <daniel.henry@epiforge.com>
2020-05-23 01:20:59 -0400

    Components: useless namespace

--- Cogs.Components/PropertyChangeNotifier.cs
+++ Cogs.Components/PropertyChangeNotifier.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.CompilerServices;
 
 namespace Cogs.Components
