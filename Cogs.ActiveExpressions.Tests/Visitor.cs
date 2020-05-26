c6248999376829047d8640e89583c09e594c88ce
Daniel Henry <daniel.henry@epiforge.com>
2020-05-26 01:46:23 -0400

    ActiveExpressions: some tests for ExpressionDiagramVisitor (more are needed)

---
+++ Cogs.ActiveExpressions.Tests/Visitor.cs
@@ -0,0 +1,77 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Cogs.ActiveExpressions.Tests
+{
+    [TestClass]
+    public class Visitor
+    {
+        [TestMethod]
+        public void VisitBinary()
+        {
+            var visitor = new ExpressionDiagramVisitor();
+            visitor.Visit((Expression<Func<int, int, int>>)((a, b) => a + b));
+            Assert.IsTrue(visitor.Elements.SequenceEqual(new object?[]
+            {
+                false,
+                ExpressionType.Lambda,
+                typeof(Func<int, int, int>),
+                typeof(int),
+                false,
+
+                false,
+                ExpressionType.Add,
+                typeof(int),
+                false,
+                false,
+                null,
+
+                false,
+                ExpressionType.Parameter,
+                typeof(int),
+                0,
+                0,
+
+                false,
+                ExpressionType.Parameter,
+                typeof(int),
+                0,
+                1,
+
+                false,
+                ExpressionType.Parameter,
+                typeof(int),
+                0,
+                0,
+
+                false,
+                ExpressionType.Parameter,
+                typeof(int),
+                0,
+                1
+            }));
+        }
+
+        [TestMethod]
+        public void VisitConstant()
+        {
+            var visitor = new ExpressionDiagramVisitor();
+            visitor.Visit((Expression<Func<int>>)(() => 1));
+            Assert.IsTrue(visitor.Elements.SequenceEqual(new object?[]
+            {
+                false,
+                ExpressionType.Lambda,
+                typeof(Func<int>),
+                typeof(int),
+                false,
+
+                false,
+                ExpressionType.Constant,
+                typeof(int),
+                1
+            }));
+        }
+    }
+}
