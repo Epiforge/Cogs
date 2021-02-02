98ec57801f29039267769bd629d6dc6ca2ec1259
Daniel Henry <daniel.henry@epiforge.com>
2021-02-02 05:44:41 -0500

    Wpf: new converter

---
+++ Cogs.Wpf/ValueConversion/NullIsFalseValueConverter.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cogs.Wpf.ValueConversion
+{
+    /// <summary>
+    /// Converts the value to <c>false</c> when <c>null</c>; otherwise, <c>true</c>
+    /// </summary>
+    public class NullIsFalseValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>A converted value</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) => value is not null;
+
+        /// <summary>
+        /// Converts a value
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>A converted value</returns>
+        public object? ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value is bool boolean && !boolean ? null : Binding.DoNothing;
+    }
+}
