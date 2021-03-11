b671aa7bef073b4bfdb655ca103437536ce26576
Daniel Henry <daniel.henry@epiforge.com>
2021-03-10 22:50:51 -0500

    Wpf: added ObjectIsTypeValueConverter; minor increment

---
+++ Cogs.Wpf/ValueConversion/ObjectIsTypeValueConverter.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Cogs.Wpf.ValueConversion
+{
+    /// <summary>
+    /// Converts the value to its corresponding <see cref="Type"/>
+    /// </summary>
+    [ValueConversion(typeof(object), typeof(Type))]
+    public class ObjectIsTypeValueConverter : IValueConverter
+    {
+        /// <summary>
+        /// Converts a value
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>A converted value</returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) =>
+            value?.GetType() ?? typeof(void);
+
+        /// <summary>
+        /// Converts a value
+        /// </summary>
+        /// <param name="value">The value produced by the binding source</param>
+        /// <param name="targetType">The type of the binding target property</param>
+        /// <param name="parameter">The converter parameter to use</param>
+        /// <param name="culture">The culture to use in the converter</param>
+        /// <returns>A converted value</returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            Binding.DoNothing;
+
+        /// <summary>
+        /// Gets a shared instance of <see cref="AnyFalseIsCollapsedMultiValueConverter"/>
+        /// </summary>
+        public static ObjectIsTypeValueConverter Default { get; } = new ObjectIsTypeValueConverter();
+    }
+}
