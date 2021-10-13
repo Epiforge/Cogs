d96df174a7a8a972fcce8b6d4a1cf5584d7a960b
Daniel Henry <daniel.henry@epiforge.com>
2021-10-13 15:24:50 -0400

    Wpf: added the IsInSafePosition extension method for windows; minor increment

--- Cogs.Wpf/WindowExtensions.cs
+++ Cogs.Wpf/WindowExtensions.cs
@@ -6,6 +6,23 @@ namespace Cogs.Wpf;
 public static class WindowExtensions
 {
     /// <summary>
+    /// Gets whether the specified window is completely contained within the closest working area
+    /// </summary>
+    /// <param name="window">The window the position for which to examine</param>
+    /// <returns><c>true</c> if the window completely contained within the closest working area; otherwise, <c>false</c></returns>
+    public static bool IsInSafePosition(this Window window)
+    {
+        var closestWorkingArea = Screen.GetWorkingArea(window);
+        return closestWorkingArea != Rect.Empty &&
+            window.Width <= closestWorkingArea.Width &&
+            window.Height <= closestWorkingArea.Height &&
+            window.Left >= closestWorkingArea.Left &&
+            window.Top >= closestWorkingArea.Top &&
+            window.Left + window.Width <= closestWorkingArea.Right &&
+            window.Top + window.Height <= closestWorkingArea.Bottom;
+    }
+
+    /// <summary>
     /// Moves the specified window the minimum amount to be completely contained within the closest working area
     /// </summary>
     /// <param name="window">The window for which to safeguard position</param>
