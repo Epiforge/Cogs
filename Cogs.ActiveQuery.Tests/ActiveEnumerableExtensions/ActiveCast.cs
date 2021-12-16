304f88f39148d0670a8c7edbc5736ee1af681876
Daniel Henry <daniel.henry@epiforge.com>
2021-12-15 21:09:58 -0500

    ActiveQuery: simplified test syntax

--- Cogs.ActiveQuery.Tests/ActiveEnumerableExtensions/ActiveCast.cs
+++ Cogs.ActiveQuery.Tests/ActiveEnumerableExtensions/ActiveCast.cs
@@ -6,7 +6,7 @@ public class ActiveCast
     [TestMethod]
     public void SourceManipulation()
     {
-        var numbers = new SynchronizedRangeObservableCollection<int>(System.Linq.Enumerable.Range(1, 10));
+        var numbers = new SynchronizedRangeObservableCollection<int>(Enumerable.Range(1, 10));
         using var query = numbers.ActiveCast<object>();
         Assert.AreEqual(0, query.GetElementFaults().Count);
         Assert.AreEqual(55, query.Cast<int>().Sum());
