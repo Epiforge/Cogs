17008cd050e5d37968bec714b88d7f23e63a822c
Daniel Henry <daniel.henry@epiforge.com>
2021-12-21 12:10:50 -0500

    Components: added the ability to pass cached event arguments to SetBackedProperty; minor increment

--- Cogs.Components/PropertyChangeNotifier.cs
+++ Cogs.Components/PropertyChangeNotifier.cs
@@ -3,7 +3,9 @@ namespace Cogs.Components;
 /// <summary>
 /// Provides a mechanism for notifying about property changes
 /// </summary>
-public abstract class PropertyChangeNotifier : INotifyPropertyChanged, INotifyPropertyChanging
+public abstract class PropertyChangeNotifier :
+    INotifyPropertyChanged,
+    INotifyPropertyChanging
 {
     /// <summary>
     /// Occurs when a property value changes
@@ -90,6 +92,28 @@ public abstract class PropertyChangeNotifier : INotifyPropertyChanged, INotifyPr
     /// <typeparam name="TValue">The type of the property</typeparam>
     /// <param name="backingField">A reference to the backing field of the property</param>
     /// <param name="value">The new value</param>
+    /// <param name="propertyChangingEventArgs">Arguments to use when raising the <see cref="PropertyChanging"/> event</param>
+    /// <param name="propertyChangedEventArgs">Arguments to use when raising the <see cref="PropertyChanged"/> event</param>
+    /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
+    [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
+    protected bool SetBackedProperty<TValue>(ref TValue backingField, TValue value, PropertyChangingEventArgs propertyChangingEventArgs, PropertyChangedEventArgs propertyChangedEventArgs)
+    {
+        if (!EqualityComparer<TValue>.Default.Equals(backingField, value))
+        {
+            OnPropertyChanging(propertyChangingEventArgs);
+            backingField = value;
+            OnPropertyChanged(propertyChangedEventArgs);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Compares a property's backing field and a new value for inequality, and when they are unequal, raises the <see cref="PropertyChanging"/> event, sets the backing field to the new value, and then raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+    /// <typeparam name="TValue">The type of the property</typeparam>
+    /// <param name="backingField">A reference to the backing field of the property</param>
+    /// <param name="value">The new value</param>
     /// <param name="propertyName">The name of the property</param>
     /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
     [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
@@ -100,6 +124,28 @@ public abstract class PropertyChangeNotifier : INotifyPropertyChanged, INotifyPr
             OnPropertyChanging(propertyName);
             backingField = value;
             OnPropertyChanged(propertyName);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Compares a property's backing field and a new value for inequality, and when they are unequal, raises the <see cref="PropertyChanging"/> event, sets the backing field to the new value, and then raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+    /// <typeparam name="TValue">The type of the property</typeparam>
+    /// <param name="backingField">A reference to the backing field of the property</param>
+    /// <param name="value">The new value</param>
+    /// <param name="propertyChangingEventArgs">Arguments to use when raising the <see cref="PropertyChanging"/> event</param>
+    /// <param name="propertyChangedEventArgs">Arguments to use when raising the <see cref="PropertyChanged"/> event</param>
+    /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
+    [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
+    protected bool SetBackedProperty<TValue>(ref TValue backingField, in TValue value, PropertyChangingEventArgs propertyChangingEventArgs, PropertyChangedEventArgs propertyChangedEventArgs)
+    {
+        if (!EqualityComparer<TValue>.Default.Equals(backingField, value))
+        {
+            OnPropertyChanging(propertyChangingEventArgs);
+            backingField = value;
+            OnPropertyChanged(propertyChangedEventArgs);
             return true;
         }
         return false;
