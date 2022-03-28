575dcaea9b0b5a262a561784ab52652934d3dc4c
Daniel Henry <daniel.henry@epiforge.com>
2022-03-28 14:34:42 -0400

    ActiveQuery: more tests; spelling correction

--- Cogs.ActiveQuery.Tests/ActiveEnumerableExtensions/ActiveConcat.cs
+++ Cogs.ActiveQuery.Tests/ActiveEnumerableExtensions/ActiveConcat.cs
@@ -4,20 +4,12 @@ namespace Cogs.ActiveQuery.Tests.ActiveEnumerableExtensions;
 public class ActiveConcat
 {
     [TestMethod]
+    [ExpectedException(typeof(InvalidOperationException))]
     public void DifferentSynchronizationContexts()
     {
-        var invalidThrown = false;
         var left = TestPerson.CreatePeopleCollection(new AsyncSynchronizationContext());
         var right = TestPerson.CreatePeopleCollection(new AsyncSynchronizationContext());
-        try
-        {
-            using var query = left.ActiveConcat(right);
-        }
-        catch (InvalidOperationException)
-        {
-            invalidThrown = true;
-        }
-        Assert.IsTrue(invalidThrown);
+        using var query = left.ActiveConcat(right);
     }
 
     [TestMethod]
@@ -48,6 +40,22 @@ public class ActiveConcat
     }
 
     [TestMethod]
+    public void SourceManipulationLeftDoesNotNotify()
+    {
+        var left = TestPerson.MakePeople();
+        var right = TestPerson.CreatePeopleCollection(new AsyncSynchronizationContext());
+        using var query = left.ActiveConcat(right);
+        Assert.AreEqual(0, query.GetElementFaults().Count);
+        Assert.AreEqual(28, query.Count);
+        right[0] = right[0];
+        right.ReplaceRange(0, 2, right.GetRange(0, 2));
+        right.Add(right[0]);
+        Assert.AreEqual(29, query.Count);
+        right.Reset(System.Linq.Enumerable.Empty<TestPerson>());
+        Assert.AreEqual(14, query.Count);
+    }
+
+    [TestMethod]
     public void SourceManipulationRightContext()
     {
         var left = TestPerson.CreatePeopleCollection(new AsyncSynchronizationContext());
@@ -72,6 +80,24 @@ public class ActiveConcat
         Assert.AreEqual(13, query.Count);
         right.Reset(System.Linq.Enumerable.Empty<TestPerson>());
         Assert.AreEqual(0, query.Count);
+    }
+
+    [TestMethod]
+    public void SourceManipulationRightDoesNotNotify()
+    {
+        var left = TestPerson.CreatePeopleCollection(new AsyncSynchronizationContext());
+        var right = TestPerson.MakePeople();
+        using var query = left.ActiveConcat(right);
+        Assert.AreEqual(0, query.GetElementFaults().Count);
+        Assert.AreEqual(28, query.Count);
+        left.RemoveAt(0);
+        Assert.AreEqual(27, query.Count);
+        left[0] = left[0];
+        left.ReplaceRange(0, 2, left.GetRange(0, 2));
+        left.Add(left[0]);
+        Assert.AreEqual(28, query.Count);
+        left.Reset(System.Linq.Enumerable.Empty<TestPerson>());
+        Assert.AreEqual(14, query.Count);
     }
 
     [TestMethod]
