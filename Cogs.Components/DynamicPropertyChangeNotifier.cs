9ee602ea820a584d25bf924006b4395430c5b314
Daniel Henry <daniel.henry@epiforge.com>
2022-05-08 16:07:57 -0400

    Components: added a version of PropertyChangeNotifier for DynamicObjects; minor increment

---
+++ Cogs.Components/DynamicPropertyChangeNotifier.cs
@@ -0,0 +1,154 @@
+namespace Cogs.Components;
+
+/// <summary>
+/// Provides a mechanism for notifying about property changes for a <see cref="DynamicObject"/>
+/// </summary>
+public class DynamicPropertyChangeNotifier :
+    DynamicObject,
+    INotifyPropertyChanged,
+    INotifyPropertyChanging
+{
+    /// <summary>
+    /// Occurs when a property value changes
+    /// </summary>
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    /// <summary>
+    /// Occurs when a property value is changing
+    /// </summary>
+    public event PropertyChangingEventHandler? PropertyChanging;
+
+    /// <summary>
+    /// Raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+	/// <param name="e">The arguments of the event</param>
+    /// <exception cref="ArgumentNullException"><paramref name="e"/> is null</exception>
+    protected virtual void OnPropertyChanged(PropertyChangedEventArgs e)
+    {
+        if (e is null)
+            throw new ArgumentNullException(nameof(e));
+        PropertyChanged?.Invoke(this, e);
+    }
+
+    /// <summary>
+    /// Notifies that a property changed
+    /// </summary>
+    /// <param name="propertyName">The name of the property that changed</param>
+	/// <exception cref="ArgumentNullException"><paramref name="propertyName"/> is null</exception>
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        if (propertyName is null)
+            throw new ArgumentNullException(nameof(propertyName));
+        OnPropertyChanged(new PropertyChangedEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Raises the <see cref="PropertyChanging"/> event
+    /// </summary>
+	/// <param name="e">The arguments of the event</param>
+    /// <exception cref="ArgumentNullException"><paramref name="e"/> is null</exception>
+    protected virtual void OnPropertyChanging(PropertyChangingEventArgs e)
+    {
+        if (e is null)
+            throw new ArgumentNullException(nameof(e));
+        PropertyChanging?.Invoke(this, e);
+    }
+
+    /// <summary>
+    /// Notifies that a property is changing
+    /// </summary>
+	/// <param name="propertyName">The name of the property that is changing</param>
+    /// <exception cref="ArgumentNullException"><paramref name="propertyName"/> is null</exception>
+    protected void OnPropertyChanging([CallerMemberName] string? propertyName = null)
+    {
+        if (propertyName is null)
+            throw new ArgumentNullException(nameof(propertyName));
+        OnPropertyChanging(new PropertyChangingEventArgs(propertyName));
+    }
+
+    /// <summary>
+    /// Compares a property's backing field and a new value for inequality, and when they are unequal, raises the <see cref="PropertyChanging"/> event, sets the backing field to the new value, and then raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+    /// <typeparam name="TValue">The type of the property</typeparam>
+    /// <param name="backingField">A reference to the backing field of the property</param>
+    /// <param name="value">The new value</param>
+    /// <param name="propertyName">The name of the property</param>
+    /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
+    [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
+    protected bool SetBackedProperty<TValue>(ref TValue backingField, TValue value, [CallerMemberName] string? propertyName = null)
+    {
+        if (!EqualityComparer<TValue>.Default.Equals(backingField, value))
+        {
+            OnPropertyChanging(propertyName);
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Compares a property's backing field and a new value for inequality, and when they are unequal, raises the <see cref="PropertyChanging"/> event, sets the backing field to the new value, and then raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+    /// <typeparam name="TValue">The type of the property</typeparam>
+    /// <param name="backingField">A reference to the backing field of the property</param>
+    /// <param name="value">The new value</param>
+    /// <param name="propertyChangingEventArgs">Arguments to use when raising the <see cref="PropertyChanging"/> event</param>
+    /// <param name="propertyChangedEventArgs">Arguments to use when raising the <see cref="PropertyChanged"/> event</param>
+    /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
+    [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
+    protected bool SetBackedProperty<TValue>(ref TValue backingField, TValue value, PropertyChangingEventArgs propertyChangingEventArgs, PropertyChangedEventArgs propertyChangedEventArgs)
+    {
+        if (!EqualityComparer<TValue>.Default.Equals(backingField, value))
+        {
+            OnPropertyChanging(propertyChangingEventArgs);
+            backingField = value;
+            OnPropertyChanged(propertyChangedEventArgs);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Compares a property's backing field and a new value for inequality, and when they are unequal, raises the <see cref="PropertyChanging"/> event, sets the backing field to the new value, and then raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+    /// <typeparam name="TValue">The type of the property</typeparam>
+    /// <param name="backingField">A reference to the backing field of the property</param>
+    /// <param name="value">The new value</param>
+    /// <param name="propertyName">The name of the property</param>
+    /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
+    [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
+    protected bool SetBackedProperty<TValue>(ref TValue backingField, in TValue value, [CallerMemberName] string? propertyName = null)
+    {
+        if (!EqualityComparer<TValue>.Default.Equals(backingField, value))
+        {
+            OnPropertyChanging(propertyName);
+            backingField = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+        return false;
+    }
+
+    /// <summary>
+    /// Compares a property's backing field and a new value for inequality, and when they are unequal, raises the <see cref="PropertyChanging"/> event, sets the backing field to the new value, and then raises the <see cref="PropertyChanged"/> event
+    /// </summary>
+    /// <typeparam name="TValue">The type of the property</typeparam>
+    /// <param name="backingField">A reference to the backing field of the property</param>
+    /// <param name="value">The new value</param>
+    /// <param name="propertyChangingEventArgs">Arguments to use when raising the <see cref="PropertyChanging"/> event</param>
+    /// <param name="propertyChangedEventArgs">Arguments to use when raising the <see cref="PropertyChanged"/> event</param>
+    /// <returns>true if <paramref name="backingField"/> was unequal to <paramref name="value"/>; otherwise, false</returns>
+    [SuppressMessage("Code Analysis", "CA1045: Do not pass types by reference", Justification = "To 'correct' this would defeat the purpose of the method")]
+    protected bool SetBackedProperty<TValue>(ref TValue backingField, in TValue value, PropertyChangingEventArgs propertyChangingEventArgs, PropertyChangedEventArgs propertyChangedEventArgs)
+    {
+        if (!EqualityComparer<TValue>.Default.Equals(backingField, value))
+        {
+            OnPropertyChanging(propertyChangingEventArgs);
+            backingField = value;
+            OnPropertyChanged(propertyChangedEventArgs);
+            return true;
+        }
+        return false;
+    }
+}
--- Cogs.Components/Using.cs
+++ Cogs.Components/Using.cs
@@ -2,4 +2,5 @@ global using System;
 global using System.Collections.Generic;
 global using System.ComponentModel;
 global using System.Diagnostics.CodeAnalysis;
+global using System.Dynamic;
 global using System.Runtime.CompilerServices;
